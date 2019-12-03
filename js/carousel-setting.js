b8252704ebe00745973fba49f67c1c772b08dc18
AndrewProkopenko <i.andrewprokopenko@gmail.com>
2019-12-03 19:21:08 +0300

    02.12 night commit

---
+++ js/carousel-setting.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $("#carousel-first").owlCarousel({
+        items: 5,
+        responsive: { 
+            0: { 
+                items: 2
+            },
+            500: { 
+                items: 3
+            },
+            767: { 
+                items: 4
+            },
+            900: { 
+                items: 5
+            }
+        }
+    });
+  });
+$(document).ready(function(){
+    $("#carousel-two").owlCarousel({
+        items: 5,
+        responsive: { 
+            0: { 
+                items: 2
+            },
+            500: { 
+                items: 3
+            },
+            767: { 
+                items: 4
+            },
+            900: { 
+                items: 5
+            }
+        }
+    });
+  });
